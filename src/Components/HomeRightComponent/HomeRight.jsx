89e8ea0f08aba5c6d1c106f23370c07bb1c8d00d
Khanhpear97 <khanhpear97@gmail.com>
2024-02-17 17:06:11 +0700

    update home right

--- src/Components/HomeRightComponent/HomeRight.jsx
+++ src/Components/HomeRightComponent/HomeRight.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import {Sugesstion} from "./Sugesstion";
 
 export const HomeRight = () => {
     return (
         <div className='border'>
-            HomeRight
+            <div className='flex justify-between items-center'>
+                <div className='flex items-center'>
+                    <div>
+                        <img className='w-12 h-12 rounded-full'
+                             src="https://cdn.pixabay.com/photo/2019/05/30/17/44/cat-4240315_1280.jpg" alt=""/>
+                    </div>
+                    <div className='ml-3'>
+                        <p>username</p>
+                        <p className='opacity-70'>nickname</p>
+                    </div>
+                </div>
+
+                <div className='text-blue-700 font-semibold'>
+                    <p>switch</p>
+                </div>
+            </div>
+            <div className='space-y-5 mt-10'>
+                {[1, 1, 1, 1].map((item) => <Sugesstion/>)}
+            </div>
         </div>
     )
 }
